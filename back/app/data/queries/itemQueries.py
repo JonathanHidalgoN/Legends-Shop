79f88e98fb7bdd7c5b1cacee497c7824cb69b430
jonathan <joni7840850@gmail.com>
2025-01-30 16:38:43 -0600

    Add two new functions to query all stats and stats names

--- back/app/data/queries/itemQueries.py
+++ back/app/data/queries/itemQueries.py
@@ -1,6 +1,7 @@
 from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from app.data.models.StatsTable import StatsTable
 from app.data.models.TagsTable import (
     TagsTable,
 )
@@ -14,8 +15,21 @@ async def getAllTagsTable(asyncSession: AsyncSession) -> List[TagsTable]:
 
 
 async def getAllTagsTableNames(asyncSession: AsyncSession) -> List[str]:
-    # Using select to query the TagsTable
     async with asyncSession.begin():
         result = await asyncSession.execute(select(TagsTable))
         existingTags: List[str] = [tag.name for tag in result.scalars().all()]
     return existingTags
+
+
+async def getAllStatsTable(asyncSession: AsyncSession) -> List[StatsTable]:
+    async with asyncSession.begin():
+        result = await asyncSession.execute(select(StatsTable))
+        existingStats: List[StatsTable] = [tag for tag in result.scalars().all()]
+    return existingStats
+
+
+async def getAllStatsTableNames(asyncSession: AsyncSession) -> List[str]:
+    async with asyncSession.begin():
+        result = await asyncSession.execute(select(StatsTable))
+        existingStats: List[str] = [tag.name for tag in result.scalars().all()]
+    return existingStats
