7dcac8fdb6ee399aa9dec84fa64a065ad24a6dd7
jonathan <joni7840850@gmail.com>
2025-02-01 10:32:38 -0600

    Add function to map from item to itemTable

--- back/app/data/mappers.py
+++ back/app/data/mappers.py
@@ -1,5 +1,6 @@
+from app.data.models.ItemTable import ItemTable
 from app.data.models.GoldTable import GoldTable
-from app.schemas.Item import Gold
+from app.schemas.Item import Gold, Item
 
 
 def mapGoldToGoldTable(gold: Gold) -> GoldTable:
@@ -20,3 +21,14 @@ def mapGoldTableToGold(goldTable: GoldTable) -> Gold:
         sell=goldTable.sell,
     )
     return gold
+
+
+def mapItemToItemTable(item: Item, goldId: int, updated: bool = True) -> ItemTable:
+    itemTable: ItemTable = ItemTable(
+        name=item.name,
+        plain_text=item.plaintext,
+        image=item.image.full,
+        gold_id=goldId,
+        updated=updated,
+    )
+    return itemTable
