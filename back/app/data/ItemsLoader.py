e9329aae01e348f4ca3de5a672bbfb1d0d53e62b
jonathan <joni7840850@gmail.com>
2025-02-04 16:21:02 -0600

    Add debug messages

--- back/app/data/ItemsLoader.py
+++ back/app/data/ItemsLoader.py
@@ -39,11 +39,13 @@ class ItemsLoader:
         self.notUpdatedItemsId: List[int] = []
 
     async def getRawJson(self) -> dict | None:
+        logger.debug("Getting json items")
         try:
             async with httpx.AsyncClient() as client:
                 response = await client.get(self.ITEMS_URL)
                 response.raise_for_status()
                 data = response.json()
+                logger.debug("Fetched json items successfully")
                 return data
         except json.JSONDecodeError as e:
             logger.exception(f"JSON decode error when getting the items json : {e}")
@@ -54,10 +56,11 @@ class ItemsLoader:
         except Exception as e:
             logger.exception(f"Exception when fetching the JSON with items : {e}")
             raise JSONFetchError(
-                "An unexpected error ocurred when fetching JSON items"
+            "An unexpected error ocurred when fetching JSON items"
             ) from e
 
     def parseRawJsonIntoItemsList(self, itemsDict: Json) -> List[Item]:
+        logger.debug("Parsing json items into a list of items")
         items: List[Item] = []
         self.version = itemsDict.get("version")
         if self.version is None:
@@ -86,9 +89,11 @@ class ItemsLoader:
                 logger.exception(
                     f"Error, the item with id {itemId} could not be parsed, exception : {e}"
                 )
+        logger.debug(f"Parsed json items into a list with {len(items)}")
         return items
 
     async def updateItems(self) -> None:
+        logger.debug("Updating items")
         itemsDict: dict | None = await self.getRawJson()
         if not itemsDict:
             raise ItemsLoaderError("Failed to fetch items JSON")
@@ -98,16 +103,22 @@ class ItemsLoader:
         succes: bool = await self.updateItemsTable(itemsList)
         if not succes:
             raise ItemsLoaderError("Failed to update items table")
+        logger.debug("Updated items successfully")
         self.updated = True
 
     async def updateTagsTable(self, tagsList: List[str]) -> bool:
         try:
             existingTagNames: List[str] = await getAllTagsTableNames(self.dbSession)
+            logger.debug(f"Updating tags table with {len(tagsList)} tags, just new tags will be added, currently {len(existingTagNames)} in the database")
+            counter : int = 0
             for tag in tagsList:
-                newTag: TagsTable = TagsTable(name=tag)
-                self.dbSession.add(newTag)
-                existingTagNames.append(tag)
+                if tag not in existingTagNames:
+                    newTag: TagsTable = TagsTable(name=tag)
+                    self.dbSession.add(newTag)
+                    existingTagNames.append(tag)
+                    counter+=1
             await self.dbSession.commit()
+            logger.debug(f"Updated tags table successfully, added {counter} new tags")
             return True
         except Exception as e:
             await self.dbSession.rollback()
@@ -117,11 +128,16 @@ class ItemsLoader:
     async def updateStatsTable(self, statsList: List[str]) -> bool:
         try:
             existingStatNames: List[str] = await getAllStatsTableNames(self.dbSession)
+            logger.debug(f"Updating stats table with {len(statsList)} stats, just new stats will be added, currently {len(existingStatNames)} in the database")
+            counter : int = 0
             for stat in statsList:
-                newStat: StatsTable = StatsTable(name=stat)
-                self.dbSession.add(newStat)
-                existingStatNames.append(stat)
+                if stat not in statsList:
+                    newStat: StatsTable = StatsTable(name=stat)
+                    self.dbSession.add(newStat)
+                    existingStatNames.append(stat)
+                    counter+=1
             await self.dbSession.commit()
+            logger.debug(f"Updated stats table successfully, added {counter} new stats")
             return True
         except Exception as e:
             await self.dbSession.rollback()
@@ -129,10 +145,10 @@ class ItemsLoader:
             raise TableUpdateError("Error updating Stats table") from e
 
     async def _addStatsRelationWithItem(
-        self, itemTable: ItemTable, stats: Stats, value: float
+        self, itemTable: ItemTable, stats: Stats
     ) -> None:
         try:
-            for stat in stats.root:
+            for stat, statValue in stats.root.items():
                 statId: int | None = await getStatIdWithStatName(self.dbSession, stat)
                 if statId is None:
                     logger.error(
@@ -144,7 +160,7 @@ class ItemsLoader:
                 itemStatValues: dict = {
                     "item_id": ItemTable.id,
                     "stat_id": statId,
-                    "value": 0,
+                    "value": statValue,
                 }
                 ins = insert(ItemStatAssociation).values(**itemStatValues)
                 await self.dbSession.execute(ins)
@@ -194,7 +210,7 @@ class ItemsLoader:
             itemTable: ItemTable = mapItemToItemTable(item, goldId, True)
             self.dbSession.add(itemTable)
             await self.dbSession.flush()
-            await self._addStatsRelationWithItem(itemTable, item.stats, 0)
+            await self._addStatsRelationWithItem(itemTable, item.stats)
             await self._addTagsRelationWithItem(itemTable, item.tags)
         except Exception as e:
             await self.dbSession.rollback()
@@ -203,6 +219,7 @@ class ItemsLoader:
 
     async def updateItemsTable(self, itemsList: List[Item]) -> bool:
         try:
+            logger.debug("Updating items table")
             tagsList: List[str] = [tag for item in itemsList for tag in item.tags]
             tagsUpdated: bool = await self.updateTagsTable(tagsList)
             statsList: List[str] = [
@@ -212,6 +229,7 @@ class ItemsLoader:
             if tagsUpdated and statsUpdated:
                 for item in itemsList:
                     await self._updateItemInTable(item)
+            logger.debug("Updated items table successfully")
             return True
         except TableUpdateError as e:
             self.updated = False
