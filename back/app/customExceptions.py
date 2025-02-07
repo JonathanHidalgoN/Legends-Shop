e625428c83ffb5b87979c0db772f51bf172976c7
jonathan <joni7840850@gmail.com>
2025-02-06 22:32:16 -0600

    Add new custom exceptions

--- back/app/customExceptions.py
+++ back/app/customExceptions.py
@@ -15,9 +15,30 @@ class NoDataNodeInJSON(ItemsLoaderError):
 
 
 class TableUpdateError(ItemsLoaderError):
-    def __init__(self, tableName:str, message:str | None = None):
+    def __init__(self, tableName: str, message: str | None = None):
         if message is None:
             message = f"Failed to update table:{tableName}"
         super().__init__(message)
         self.tableName = tableName
+
+    pass
+
+
+class JsonFetchError(ItemsLoaderError):
+    pass
+
+
+class JsonParseError(ItemsLoaderError):
+    pass
+
+
+class UpdateTagsError(ItemsLoaderError):
+    pass
+
+
+class UpdateStatsError(ItemsLoaderError):
+    pass
+
+
+class UpdateItemsError(ItemsLoaderError):
     pass
