cd4ffcbab8b956b7d7dd4f0702a96bdd9353a75c
jonathan <joni7840850@gmail.com>
2025-02-08 17:25:40 -0600

    Delete some exceptions

--- back/app/customExceptions.py
+++ back/app/customExceptions.py
@@ -1,29 +1,6 @@
 class ItemsLoaderError(Exception):
     pass
 
-
-class JSONFetchError(ItemsLoaderError):
-    pass
-
-
-class DataValidationError(ItemsLoaderError):
-    pass
-
-
-class NoDataNodeInJSON(ItemsLoaderError):
-    pass
-
-
-class TableUpdateError(ItemsLoaderError):
-    def __init__(self, tableName: str, message: str | None = None):
-        if message is None:
-            message = f"Failed to update table:{tableName}"
-        super().__init__(message)
-        self.tableName = tableName
-
-    pass
-
-
 class JsonFetchError(ItemsLoaderError):
     pass
 
