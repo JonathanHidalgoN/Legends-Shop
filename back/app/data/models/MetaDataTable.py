a1fde3b1e1d034879d842c966d5f64e4edff0c8b
jonathan <joni7840850@gmail.com>
2025-02-10 12:59:55 -0600

    Add meta data table

---
+++ back/app/data/models/MetaDataTable.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from app.data.database import base
+
+
+class MetaDataTable(base):
+    __tablename__ = "meta_data_table"
+    field_name: Mapped[str] = mapped_column(primary_key=True, nullable=False, unique=True)
+    value: Mapped[str] = mapped_column(nullable=False)
+
