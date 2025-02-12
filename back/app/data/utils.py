7f43102735c13204505138bfcfc99432e218587a
jonathan <joni7840850@gmail.com>
2025-02-12 07:27:13 -0600

    Add functions to get all itemTAble rows and convert into Items

--- back/app/data/utils.py
+++ back/app/data/utils.py
@@ -1,13 +1,43 @@
-import os
-from dotenv import load_dotenv
+from typing import Dict, List, Set
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.data.mappers import mapGoldTableToGold, mapItemTableToItem
+from app.data.models.GoldTable import GoldTable
+from app.data.models.ItemTable import ItemTable
+from app.data.queries.itemQueries import getAllEffectNamesAndValueAssociatedByItemId, getAllStatNamesAndValueAssociatedByItemId, getAllTagNamesAssociatedByItemId, getGoldTableWithId, getItems
+from app.schemas.Item import Effects, Gold, Image, Item, Stats
 
-# This function gets the databaseUrl
-def getDatabaseUrl() -> str:
-    load_dotenv()
-    databaseUrl = os.getenv("DATABASE_URL", "Empty")
-    return databaseUrl
+async def getAllItemTableRowsAnMapToItems(asyncSession: AsyncSession)->List[Item]:
+    """
+    Get all itemTable rows and map to Item objects
+    """
+    items: List[Item] = []
+    itemTableRows: List[ItemTable] = await getItems(asyncSession)
+    for itemTable in itemTableRows:
+        item : Item = await convertItemTableIntoItem(asyncSession, itemTable)
+        items.append(item)
+    return items
 
-
-##Run the app with this function and check if bracks, else check
-# the alembic command to update with alchemy modesl
+async def convertItemTableIntoItem(asyncSession : AsyncSession, itemTable: ItemTable) -> Item:
+    """
+    This function takes an itemTable and get all the relations wiht gold,effects,stats and tags.
+    Then creates an Item object
+    """
+    goldTable : GoldTable | None = await getGoldTableWithId(asyncSession,itemTable.gold_id) 
+    gold : Gold = mapGoldTableToGold(goldTable)
+    tags : Set[str] = await getAllTagNamesAssociatedByItemId(asyncSession,itemTable.id)
+    statsData : Dict[str, int | float] = await getAllStatNamesAndValueAssociatedByItemId(
+        asyncSession,itemTable.id
+    ) 
+    effectsData : Dict[str, int | float]  = await getAllEffectNamesAndValueAssociatedByItemId(
+        asyncSession, itemTable.id
+    )
+    stats:Stats = Stats(root=statsData)
+    effects : Effects =  Effects(root=effectsData)
+    image : Image = Image(
+        full=itemTable.image,
+        sprite="",
+        group=""
+    )
+    return mapItemTableToItem(itemTable,gold,tags,stats,effects,image)
+        
