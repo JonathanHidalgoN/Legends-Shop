90e988e824d055a755374a3f7a19e135fa1710a3
jonathan <joni7840850@gmail.com>
2025-02-12 10:31:33 -0600

    Add logger warning in some queries

--- back/app/logger.py
+++ back/app/logger.py
@@ -3,6 +3,8 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+formatter = logging.Formatter(
+    "%(asctime)s- %(name)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s"
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
