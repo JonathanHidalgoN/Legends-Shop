36d2e647a871851d52e5b3e59973e198e73fad6e
jonathan <joni7840850@gmail.com>
2025-02-13 15:17:26 -0600

    Add file where logs are stored

--- back/app/logger.py
+++ back/app/logger.py
@@ -1,10 +1,18 @@
 import logging
+import os
+from datetime import datetime
 
+logDirName : str = "backend_logs"
+os.makedirs(logDirName,exist_ok=True)
+logFile = os.path.join(logDirName, f"logs-{datetime.now().strftime("%Y-%m-%d")}.txt")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
+streamHandler = logging.StreamHandler()
+fileHandler = logging.FileHandler(logFile)
 formatter = logging.Formatter(
     "%(asctime)s- %(name)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s"
 )
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+streamHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+logger.addHandler(fileHandler)
