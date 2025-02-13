d22204fa35d4f9a8df39a6228c1fc39728caa5ff
jonathan <joni7840850@gmail.com>
2025-02-13 07:56:34 -0600

    Make description col more characters in items table

--- back/app/data/models/ItemTable.py
+++ back/app/data/models/ItemTable.py
@@ -9,7 +9,7 @@ class ItemTable(base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
     plain_text: Mapped[str] = mapped_column(String(300), nullable=False)
-    description: Mapped[str] = mapped_column(String(300), nullable=True)
+    description: Mapped[str] = mapped_column(String(1500), nullable=False)
     image: Mapped[str] = mapped_column(String(100), nullable=False)
     imageUrl: Mapped[str] = mapped_column(String(100), nullable=False)
     updated: Mapped[bool] = mapped_column(Boolean, nullable=False)
