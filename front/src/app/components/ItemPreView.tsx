54d0f1fcee98bb44070b4090599ccf29827ab088
jonathan <joni7840850@gmail.com>
2025-02-17 08:30:14 -0600

    Improve desing

--- front/src/app/components/ItemPreView.tsx
+++ front/src/app/components/ItemPreView.tsx
@@ -1,51 +1,71 @@
-"use client"
-// components/ItemPreView.jsx
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { Item } from "../interfaces/Item";
+import { kanit, sigmar } from "../fonts";
 
-export default function ItemPreView({
-  item,
-  linkHref = "/",
-}) {
-  const targetLink = linkHref || `/items/${item.name}`;
+export default function ItemPreView({ item }: { item: Item }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const targetLink = `/items/${item.name}`;
 
   return (
-    <Link href={targetLink}>
-      {/* Set the container as relative so that the detail panel can be absolutely positioned */}
-      <div className="m-6 group block relative cursor-pointer">
-        {/* The square container with the item image */}
+    <div className="m-6 relative border border-[var(--yellow)]">
+      <Link href={targetLink}>
         <div
-          className={`relative w-64 h-64 rounded overflow-hidden shadow-md`}
+          className="block cursor-pointer"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          <Image
-            src={item.img}
-            alt={item.name}
-            layout="fill"
-            objectFit="cover"
-          />
-          {/* Overlay for the item name */}
-          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-2">
-            <p className="text-white text-center font-bold">{item.name}</p>
+          {/* The square container with the item image */}
+          <div className="relative w-40 h-40 rounded overflow-hidden shadow-md">
+            <Image
+              src={item.img}
+              alt={item.name}
+              fill
+              sizes="(max-width: 768px) 25vw, (max-width: 1200px) 25vw, 25vw"
+              quality={100}
+            />
+          </div>
+
+          {/* Price below the image */}
+          <div className="mt-2 text-center">
+            <span className={`text-xl text-[var(--yellow)] ${kanit.className}`}>
+              {item.gold.base}</span>
           </div>
         </div>
+      </Link>
 
-        {/* Detail panel which is hidden by default and appears on hover */}
+      {isHovered && (
         <div
-          className={`border border-black absolute left-0 w-full z-10 p-4 rounded bg-[var(--white)] opacity-0 group-hover:opacity-100 transition-opacity duration-300`}
-          style={{ top: "100%" }}
+          className="border border-black absolute left-full w-80 z-10 p-4 rounded bg-[var(--white)] transition-opacity duration-300"
+          style={{ top: 0 }}
         >
+          {/* Item Name at the top of the hover panel */}
+          <div className="mb-2">
+            <span className={`text-[var(--orange)] text-xl ${sigmar.className}`}>
+              {item.name}</span>
+          </div>
+
           {/* Gold Information */}
           <div className="text-[var(--extra)]">
-            <span className="font-bold text-[var(--black)]">Cost:</span> {item.gold.base}
+            <span className="font-bold text-[var(--black)]">Cost: </span>
+            <span className={`text-[var(--yellow)] text-xl ${kanit.className}`}>
+              {item.gold.base}
+            </span>
           </div>
+
           {/* Stats List */}
           {item.stats && item.stats.length > 0 && (
             <div className="mt-2">
               <span className="font-bold text-[var(--black)]">Stats:</span>
               <ul className="list-disc list-inside">
                 {item.stats.map((stat, index) => (
-                  <li key={index} className="text-[var(--extra)]">
-                    {stat.value}{stat.type === 0 ? "" : "%"} {stat.name}
+                  <li key={index} className="text-[var(--black)]">
+                    <span className={`text-[var(--orange)] ${kanit.className}`}>
+                      {stat.value}
+                    </span>
+                    {stat.kind === 0 ? "" : "%"} {stat.name}
                   </li>
                 ))}
               </ul>
@@ -58,8 +78,11 @@ export default function ItemPreView({
               <span className="font-bold text-black">Effects:</span>
               <ul className="list-disc list-inside">
                 {item.effects.map((effect, index) => (
-                  <li key={index} className="text-[var(--extra)]">
-                    {effect.value} {effect.name}
+                  <li key={index} className="text-[var(--black)]">
+                    <span className={`text-[var(--pink1)] ${kanit.className}`}>
+                      {effect.value}{' '}
+                    </span>
+                    {effect.name}
                   </li>
                 ))}
               </ul>
@@ -67,12 +90,12 @@ export default function ItemPreView({
           )}
 
           {/* Description */}
-          <div className="mt-2 text-[var(--extra)]">
-            <span className="font-bold text-[var(--black)]">Description:</span>
+          <div className="mt-2 text-[var(--black)]">
+            <span className="font-bold">Description:</span>
             {item.description}
           </div>
         </div>
-      </div>
-    </Link>
+      )}
+    </div>
   );
 }
