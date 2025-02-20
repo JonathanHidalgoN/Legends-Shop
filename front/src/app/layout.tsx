46a01cc0a619949abb9549a20acb9d1efa876385
jonathan <joni7840850@gmail.com>
2025-02-19 22:20:31 -0600

    Fetch static data here

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,17 +1,32 @@
 import Header from "./components/Header";
 import "./styles.css"
 import "./globals.css";
+import { Item } from "./interfaces/Item";
+import { fetchItems, fetchTags } from "./itemsFetcher";
+import { StaticDataContextProvider } from "./components/StaticDataContext";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  let items: Item[] = [];
+  let tags: string[] = [];
+  try {
+    items = await fetchItems();
+    tags = await fetchTags();
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
     <html lang="en">
       <body>
-        <Header />
-        {children}
+        <StaticDataContextProvider items={items} tags={tags}>
+          <Header />
+          {children}
+        </StaticDataContextProvider>
       </body>
     </html>
   );
