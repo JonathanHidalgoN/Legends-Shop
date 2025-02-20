eadc261a6a83f82ac76771921e7d075376b04ea6
jonathan <joni7840850@gmail.com>
2025-02-19 22:19:47 -0600

    Delete html code and import filtered items

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,34 +1,13 @@
-import ItemPreView from "./components/ItemPreView";
-import MainLateralPanel from "./components/MainLateralPanel";
-import { Item } from "./interfaces/Item";
-import { fetchItems, fetchTags } from "./itemsFetcher";
+"use client"
+import SelectedItems from "./components/SelectedItems";
+import { useStaticData } from "./components/StaticDataContext";
 
-export default async function Home() {
-  let items: Item[] = [];
-  let tags: string[] = [];
-  try {
-    items = await fetchItems();
-    tags = await fetchTags();
-  } catch (error) {
-    console.log(error);
-    return <div>Error</div>;
-  }
+export default function Home() {
+  const { items, tags } = useStaticData();
 
   if (!items || items.length === 0) {
     return <div>Error</div>;
   }
 
-  console.log(tags)
-
-  return (
-    <div className="grid grid-cols-[15%_auto] h-full">
-      <MainLateralPanel tags={tags} maxPrice={10000} />
-
-      <div className="p-4 m-6 flex flex-wrap gap-x-10">
-        {items.map((item, index) => (
-          <ItemPreView key={index} item={item} />
-        ))}
-      </div>
-    </div>
-  );
+  return <SelectedItems items={items} tags={tags} />;
 }
