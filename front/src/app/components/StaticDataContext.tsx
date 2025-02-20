6ce323f33a0ee00515357c0d70ea08abb7ce0d51
jonathan <joni7840850@gmail.com>
2025-02-19 22:19:26 -0600

    Add component to share context

---
+++ front/src/app/components/StaticDataContext.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React, { createContext, useContext } from "react"
+import { Item } from "../interfaces/Item"
+
+interface StaticDataContextType {
+  items: Item[];
+  tags: string[];
+}
+
+const StaticDataContext = createContext<StaticDataContextType | undefined>(undefined);
+
+export function StaticDataContextProvider({ items, tags, children }: {
+  items: Item[]; tags: string[]; children: React.ReactNode;
+}) {
+  return (
+    <StaticDataContext.Provider value={{ items, tags }}>
+      {children}
+    </StaticDataContext.Provider>
+  );
+}
+
+export function useStaticData() {
+  const context = useContext(StaticDataContext);
+  if (!context) {
+    throw new Error("useItems error");
+  }
+  return context;
+}
