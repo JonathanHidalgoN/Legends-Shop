0274a5868940da5c934b5c6734d0a80f884d4dc6
jonathan <joni7840850@gmail.com>
2025-02-22 14:43:58 -0600

    Add auth initial schemas

---
+++ back/app/schemas/AuthSchemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    userName:str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    userName:str | None = None
+
+class UserInDB(User):
+    hashedPassword: str
