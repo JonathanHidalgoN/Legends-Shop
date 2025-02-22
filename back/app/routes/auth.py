195416f1e17cd8899b53ce4875ca2f96f4cbf064
jonathan <joni7840850@gmail.com>
2025-02-22 15:50:44 -0600

    Add route to sing up

--- back/app/routes/auth.py
+++ back/app/routes/auth.py
@@ -3,20 +3,13 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.functions import createAccessToken, verifyPassword, verifyToken, hashPassword
 from app.data import database
-from app.data.queries.authQueries import getUserInDB
+from app.data.queries.authQueries import checkUserExistInDB, getUserInDB, insertUser
 from app.schemas.AuthSchemas import Token, UserInDB 
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 
 #Source:https://fastapi.tiangolo.com/tutorial/security/first-steps/#create-mainpy
 router = APIRouter()
 oauth2Scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
-
-fakeDB = {
-    "admin":{
-        "username":"admin",
-        "hashed_password":hashPassword("secret")
-    }
-}
 
 def getCurrentUser(token:Annotated[str, Depends(oauth2Scheme)]):
     userName: str | None = verifyToken(token)
@@ -31,11 +24,11 @@ def getCurrentUser(token:Annotated[str, Depends(oauth2Scheme)]):
 # https://stackoverflow.com/questions/65059811/what-does-depends-with-no-parameter-do
 @router.post("/token", response_model=Token)
 async def getToken(dataForm: Annotated[OAuth2PasswordRequestForm,  Depends()],
-                   db: AsyncSession = Depends(database.getDbSession),):
+                   db: AsyncSession = Depends(database.getDbSession)):
     try:
         matchUser : UserInDB | None = await getUserInDB(db, dataForm.username)
     except Exception:
-        raise HTTPException(status_code=503, detail="Error retriving the user from the server")
+        raise HTTPException(status_code=500, detail="Error retriving the user from the server")
     if not matchUser:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     if not verifyPassword(dataForm.password, matchUser.hashedPassword):
@@ -46,3 +39,18 @@ async def getToken(dataForm: Annotated[OAuth2PasswordRequestForm,  Depends()],
 @router.get("/test")
 async def protectTest(user:Annotated[str, Depends(getCurrentUser)]):
     return {"message":user}
+
+@router.get("/singUp")
+async def singUp(db: AsyncSession = Depends(database.getDbSession)):
+    try:
+        userExist : bool = await checkUserExistInDB(db, "user")
+    except Exception:
+        raise HTTPException(status_code=400, detail="Error retriving the user from the server")
+    if userExist:
+        raise HTTPException(status_code=500, detail="Username exist, change it")
+    userInDB: UserInDB = UserInDB(userName="user", hashedPassword=hashPassword("password"))
+    try:
+        await insertUser(db,userInDB)
+        return {"message":"nice"}
+    except Exception:
+        raise HTTPException(status_code=500, detail="Server error inserting the user")
