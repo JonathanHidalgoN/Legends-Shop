7121980edb685894a35cb4f2af1a012b64df4e04
jonathan <joni7840850@gmail.com>
2025-02-25 07:51:16 -0600

    Add logout and profile button on user button and funcitonality to hide when click outside

--- front/src/app/components/Header.tsx
+++ front/src/app/components/Header.tsx
@@ -1,16 +1,39 @@
 "use client"
-import { useState } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { sigmar } from "../fonts";
 import SearchBar from "./SearchBar";
 import { Item } from "../interfaces/Item";
 import { useAuth } from "./AuthContext";
+import { useRouter } from "next/navigation";
 
 export default function Header({ items }:
   { items: Item[] }) {
   const [showLoginDropdown, setShowLoginDropdown] = useState(false);
   const [showCartDropdown, setShowCartDropdown] = useState(false);
   const { userName } = useAuth();
+  const router = useRouter();
+  const containerRef: RefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(null);
+
+
+  //This is a compy of a function? Can I create a general one?
+  // Hide suggestions when clicking outside
+  // source: https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        setShowLoginDropdown(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <header
@@ -25,9 +48,6 @@ export default function Header({ items }:
           Legends Shop
         </Link>
       </div>
-
-
-
       <div className="flex-1 mx-4">
         <SearchBar items={items} />
       </div>
@@ -35,14 +55,40 @@ export default function Header({ items }:
       <div className="flex items-center space-x-4">
         <div className="relative">
           <button
-            onClick={() => setShowLoginDropdown((prev) => !prev)}
+            onClick={() => {
+              if (userName) {
+                setShowLoginDropdown((prev) => !prev)
+              }
+              else {
+                router.push("/auth/login");
+              }
+            }}
             className="p-2 rounded hover:opacity-80 transition"
             style={{ backgroundColor: 'var(--orange)', color: 'var(--white)' }}
           >
             {userName ? "Welcome " + userName : "Login"}
           </button>
-          {showLoginDropdown && (
-            <div className="absolute right-0 mt-2 w-40 p-2 rounded shadow-lg bg-white z-10">
+          {userName && showLoginDropdown && (
+            <div ref={containerRef}
+              className="absolute right-0 mt-2 w-40 p-2 rounded shadow-lg bg-white z-10">
+              <button
+                onClick={() => {
+                  router.push("/profile");
+                  setShowLoginDropdown(false);
+                }}
+                className="block w-full text-left px-2 py-1 hover:bg-gray-100"
+              >
+                Profile
+              </button>
+              <button
+                onClick={() => {
+                  // handleLogout();
+                  setShowLoginDropdown(false);
+                }}
+                className="block w-full text-left px-2 py-1 hover:bg-gray-100"
+              >
+                Logout
+              </button>
             </div>
           )}
         </div>
@@ -82,7 +128,7 @@ export default function Header({ items }:
         </div>
 
       </div>
-    </header>
+    </header >
   );
 };
 
