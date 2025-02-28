55263e3d51b5001ec8bcbfb6664902f64569b068
jonathan <joni7840850@gmail.com>
2025-02-27 22:39:04 -0600

    Change order type attributes

--- back/app/schemas/Order.py
+++ back/app/schemas/Order.py
@@ -1,9 +1,16 @@
 from datetime import datetime
+from typing import List
 from pydantic import BaseModel
+
+from app.schemas.Item import Item
+
+class OrderItem(BaseModel):
+    name:str
+    cost:int
 
 class Order(BaseModel):
     id: int
-    userId: int 
-    itemId: int
+    items: List[OrderItem]
+    userName: str
     total: int
-    orderDate: datetime
+    date: datetime
