9ae9b873e08439571fff25ab1fe8199195dec8b0
jonathan <joni7840850@gmail.com>
2025-02-27 22:38:37 -0600

    Add some test methods

--- back/app/routes/orders.py
+++ back/app/routes/orders.py
@@ -4,13 +4,31 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.logger import logger
 from app.data import database
 from app.routes.auth import getCurrentUserTokenFlow
+from app.schemas.Order import Order
 
 router = APIRouter()
 
-@router.get("/order")
+@router.post("/order")
+async def order(
+    request: Request, 
+    order: Order,
+    userName: Annotated[str, Depends(getCurrentUserTokenFlow)],
+    db: AsyncSession = Depends(database.getDbSession)
+):
+    return {"message": f"{order} {userName}"}
+
+
+@router.post("/test")
 async def test(
     request: Request, 
     userName: Annotated[str, Depends(getCurrentUserTokenFlow)],
     db: AsyncSession = Depends(database.getDbSession)
 ):
     return {"message": f"Hello {userName}"}
+
+@router.post("/test2")
+async def test2(
+    userName: Annotated[str, Depends(getCurrentUserTokenFlow)],
+):
+    return {"message": f"{userName}"}
+
