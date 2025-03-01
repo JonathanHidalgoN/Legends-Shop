c97767ee0b5353e673ac815c490c7fc69176ba69
jonathan <joni7840850@gmail.com>
2025-03-01 16:08:28 -0600

    Change item order to use just item names

--- back/app/schemas/Order.py
+++ back/app/schemas/Order.py
@@ -2,13 +2,9 @@ from datetime import datetime
 from typing import List
 from pydantic import BaseModel
 
-class OrderItem(BaseModel):
-    name:str
-    cost:int
-
 class Order(BaseModel):
     id: int
-    items: List[OrderItem]
+    itemNames: List[str]
     userName: str
     total: int
     date: datetime
--- front/src/app/interfaces/Order.ts
+++ front/src/app/interfaces/Order.ts
@@ -1,22 +1,8 @@
-import { Item } from "./Item";
-
-export interface OrderItem {
-  name: string,
-  cost: number
-}
-
 export interface Order {
   id: number,
   status: string
-  items: OrderItem[],
+  itemNames: string[],
   total: number,
   userName: string,
   date: Date
-}
-
-export default function mapItemToOrderItem(item: Item): OrderItem {
-  return {
-    name: item.name,
-    cost: item.gold.base
-  };
 }
