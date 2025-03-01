9be62f717ed0ae87f1e94e51907b9c3281f540f2
jonathan <joni7840850@gmail.com>
2025-03-01 12:05:52 -0600

    Add login verification when buy

--- front/src/app/order/page.tsx
+++ front/src/app/order/page.tsx
@@ -1,19 +1,21 @@
 "use client";
+import toast from "react-hot-toast";
 import { useAuthContext } from "../components/AuthContext";
 import { useCarContext } from "../components/CarContext";
 import CarDropDown from "../components/CarDropDown";
 import { Order } from "../interfaces/Order";
 import { orderRequest } from "../request";
+import { useRouter } from "next/navigation";
 
 export default function OrderPage() {
   const { carItems, getTotalCost, cleanCar } = useCarContext();
   const { userName } = useAuthContext();
-
+  const router = useRouter();
 
   async function handleBuy() {
     if (!userName) {
-      //Null userName handle
-      console.log("Null userName");
+      toast.error("Login to order");
+
     } else {
       const order: Order = {
         items: carItems,
@@ -42,17 +44,29 @@ export default function OrderPage() {
       <CarDropDown tiny={false} />
       <div className="mt-6 border-t pt-4">
         <div className="flex space-x-4">
-          <button
-            onClick={handleBuy}
-            className="flex-1 bg-[var(--orange)] text-white py-2 
-            rounded hover:bg-orange-700 transition-colors"
-          >
-            Buy
-          </button>
+          {userName && (
+            <button
+              onClick={handleBuy}
+              className="flex-1 bg-[var(--orange)] text-white py-2 
+                 rounded hover:bg-orange-700 transition-colors"
+            >
+              Buy
+            </button>
+          )}
+          {!userName && (
+            <button
+              onClick={() => {
+                router.push("/auth/login");
+              }}
+              className="flex-1 bg-[var(--orange)] text-white py-2 
+                 rounded hover:bg-orange-700 transition-colors"
+            >
+              Login
+            </button>
+          )}
           <button
             onClick={handleCancel}
-            className="flex-1 bg-gray-500
-            text-white py-2 rounded hover:bg-gray-800 transition-colors"
+            className="flex-1 bg-gray-500 text-white py-2 rounded hover:bg-gray-800 transition-colors"
           >
             Cancel
           </button>
