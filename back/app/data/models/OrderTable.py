70ef71afa1c4c5c15ed3706700760ccca7309118
jonathan <joni7840850@gmail.com>
2025-03-01 16:07:35 -0600

    Add many to many relation in orders table

--- back/app/data/models/OrderTable.py
+++ back/app/data/models/OrderTable.py
@@ -1,14 +1,25 @@
-from sqlalchemy import Date, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Table, Date, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.data.models.ItemTable import ItemTable
 from app.data.database import base
 
+OrderItemAssociation = Table(
+    "order_item_association",
+    base.metadata,
+    Column("order_id", ForeignKey("order_table.id"), primary_key=True),
+    Column("item_id", ForeignKey("item_table.id"), primary_key=True),
+    Column("quantity", Integer, nullable=False)
+)
 
 class OrderTable(base):
     __tablename__ = "order_table"
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-    item_id: Mapped[int] = mapped_column(ForeignKey("item_table.id"), nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"), nullable=False)
     total: Mapped[int] = mapped_column(nullable=False)
     order_date: Mapped[Date] = mapped_column(Date, nullable=False)
 
+    items: Mapped[list["ItemTable"]] = relationship(
+        "ItemTable",
+        secondary=OrderItemAssociation,
+    )
