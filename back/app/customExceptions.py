0801e9c1d4510abaf22f5534d4ae8774a4905c77
jonathan <joni7840850@gmail.com>
2025-03-01 17:30:13 -0600

    Add custom exceptions for order processing

--- back/app/customExceptions.py
+++ back/app/customExceptions.py
@@ -1,7 +1,6 @@
 class ItemsLoaderError(Exception):
     pass
 
-
 class JsonFetchError(ItemsLoaderError):
     pass
 
@@ -23,4 +22,24 @@ class UpdateEffectsError(ItemsLoaderError):
 
 
 class UpdateItemsError(ItemsLoaderError):
+    pass
+
+class ProcessOrderException(Exception):
+    pass
+
+class InvalidItemException(ProcessOrderException):
+    pass
+
+class UserIdNotFound(Exception):
+    def __init__(self, userName:str, message:str):
+        self.userName = userName
+        self.message = message
+        super().__init__(message)
+    pass
+
+class DifferentTotal(ProcessOrderException):
+    def __init__(self, originalTotal, databaseTotal, message):
+        self.originalTotal = originalTotal
+        self.databaseTotal = databaseTotal
+        super().__init__(message)
     pass
