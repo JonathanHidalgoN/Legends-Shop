6152c573800bb4287f645e580049b5f1824a1b2c
jonathan <joni7840850@gmail.com>
2025-03-04 09:11:57 -0600

    Add status and delivery date cols, change date to order date name

--- back/app/data/models/OrderTable.py
+++ back/app/data/models/OrderTable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Table, Date, ForeignKey, Integer
+from sqlalchemy import Column, String, Table, Date, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.data.models.ItemTable import ItemTable
 from app.data.database import base
@@ -19,6 +19,8 @@ class OrderTable(base):
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"), nullable=False)
     total: Mapped[int] = mapped_column(nullable=False)
     order_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    delivery_date: Mapped[Date] = mapped_column(Date, nullable=False)
+    status: Mapped[str] = mapped_column(String(30), nullable=False)
 
     items: Mapped[list["ItemTable"]] = relationship(
         "ItemTable",
