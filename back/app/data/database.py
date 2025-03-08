3df1c42748495ceabdbd2384a02d10bab4611d6b
jonathan <joni7840850@gmail.com>
2025-03-08 11:55:22 -0600

    Import declarative base from new module

--- back/app/data/database.py
+++ back/app/data/database.py
@@ -1,9 +1,6 @@
 from typing import AsyncGenerator
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-import os
-from dotenv import load_dotenv
+from sqlalchemy.orm import declarative_base
 from app.envVariables import DATABASE_URL
 
 
