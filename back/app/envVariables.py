012cc6c62741e4a6055eda524c588a9762ed8258
jonathan <joni7840850@gmail.com>
2025-03-08 22:22:21 -0600

    Add defautl

--- back/app/envVariables.py
+++ back/app/envVariables.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_ALEMBIC_URL: str = os.getenv("DATABASE_ALEMBIC_URL", "postgresql://db:1234@$localhost:2345/BD")
-DATABASE_URL: str = os.getenv("DATABASE_URL", "Empty")
+DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://db:1234@$localhost:2345/BD")
 FRONTEND_HOST: str = os.getenv("FRONTEND_HOST", "Empty")
 FRONTEND_PORT: str = os.getenv("FRONTEND_PORT", "Empty")
 SECRET_KEY: str = os.getenv("SECRET_KEY", "Empty")
