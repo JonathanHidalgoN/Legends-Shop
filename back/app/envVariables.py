b4f42feeb6f601016a9567300a0409a80bd9c04d
jonathan <joni7840850@gmail.com>
2025-03-08 22:27:48 -0600

    Changes

--- back/app/envVariables.py
+++ back/app/envVariables.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_ALEMBIC_URL: str = os.getenv("DATABASE_ALEMBIC_URL", "postgresql://db:1234@$localhost:2345/BD")
-DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://db:1234@$localhost:2345/BD")
+DATABASE_ALEMBIC_URL: str = os.getenv("DATABASE_ALEMBIC_URL", "postgresql+asyncpg://db:1234@$localhost:2345/BD")
+DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://db:1234@$localhost:2345/BD")
 FRONTEND_HOST: str = os.getenv("FRONTEND_HOST", "Empty")
 FRONTEND_PORT: str = os.getenv("FRONTEND_PORT", "Empty")
 SECRET_KEY: str = os.getenv("SECRET_KEY", "Empty")
