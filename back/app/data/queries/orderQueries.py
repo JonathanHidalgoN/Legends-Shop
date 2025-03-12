0fc8db6e1c2841dd6f200000fe4ff2b944f083d3
jonathan <joni7840850@gmail.com>
2025-03-12 15:10:26 -0600

    Add new filters to order history query

--- back/app/data/queries/orderQueries.py
+++ back/app/data/queries/orderQueries.py
@@ -1,4 +1,5 @@
-from typing import List
+from datetime import date
+from typing import List, Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -9,9 +10,19 @@ from app.data.models.OrderTable import OrderItemAssociation, OrderTable
 from app.schemas.Order import Order, OrderStatus
 
 
-async def getOrderHistoryQuery(asyncSession: AsyncSession, userId: int) -> List[Order]:
+async def getOrderHistoryQuery(asyncSession: AsyncSession, userId: int,
+    orderStatus: str = "ALL",
+    minOrderDate: Optional[date] = None,
+    maxOrderDate: Optional[date] = None,
+    minDeliveryDate: Optional[date] = None,
+    maxDeliveryDate: Optional[date] = None,
+    sortField: Optional[str] = None,
+    sortOrder: Optional[str] = None,
+    filterItemNames: Optional[List[str]] = None 
+
+                               ) -> List[Order]:
     """"""
-    result = await asyncSession.execute(
+    query = (
         select(
             OrderTable.id,
             OrderTable.total,
@@ -20,13 +31,56 @@ async def getOrderHistoryQuery(asyncSession: AsyncSession, userId: int) -> List[
             OrderTable.status,
             ItemTable.name,
             UserTable.userName,
-            OrderItemAssociation.c.quantity
+            OrderItemAssociation.c.quantity,
         )
         .join(OrderItemAssociation, OrderTable.id == OrderItemAssociation.c.order_id)
         .join(ItemTable, OrderItemAssociation.c.item_id == ItemTable.id)
         .join(UserTable, UserTable.id == userId)
         .where(OrderTable.user_id == userId)
     )
+
+    filters = []
+    
+    if orderStatus != "ALL":
+        filters.append(OrderTable.status == orderStatus)
+    if minOrderDate:
+        filters.append(OrderTable.order_date >= minOrderDate)
+    if maxOrderDate:
+        filters.append(OrderTable.order_date <= maxOrderDate)
+    if minDeliveryDate:
+        filters.append(OrderTable.delivery_date >= minDeliveryDate)
+    if maxDeliveryDate:
+        filters.append(OrderTable.delivery_date <= maxDeliveryDate)
+    if filterItemNames:
+        filters.append(ItemTable.name.in_(filterItemNames))
+
+    if filters:
+        query = query.where(*filters)
+
+    if sortField:
+        if sortField == "price":
+            if sortOrder == "desc":
+                query = query.order_by(OrderTable.total.desc())
+            else:
+                query = query.order_by(OrderTable.total.asc())
+        elif sortField == "orderDate":
+            if sortOrder == "desc":
+                query = query.order_by(OrderTable.order_date.desc())
+            else:
+                query = query.order_by(OrderTable.order_date.asc())
+        elif sortField == "deliveryDate":
+            if sortOrder == "desc":
+                query = query.order_by(OrderTable.delivery_date.desc())
+            else:
+                query = query.order_by(OrderTable.delivery_date.asc())
+        elif sortField == "quantity":
+            if sortOrder == "desc":
+                query = query.order_by(OrderItemAssociation.c.quantity.desc())
+            else:
+                query = query.order_by(OrderItemAssociation.c.quantity.asc())
+
+    result = await asyncSession.execute(query)
+
     rows = result.all()
     orders_dict = {}
     for (
