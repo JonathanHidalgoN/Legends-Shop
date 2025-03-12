46bbe379e9655e666c3174316427fba01a75a65e
jonathan <joni7840850@gmail.com>
2025-03-12 13:36:35 -0600

    Add filters in order history

--- front/src/app/components/OrderHistory.tsx
+++ front/src/app/components/OrderHistory.tsx
@@ -4,10 +4,47 @@ import { Order } from "@/app/interfaces/Order";
 import OrderHistoryCard from "@/app/components/OrderHistoryCard";
 import { getUserHistoryRequest } from "@/app/request";
 import toast from "react-hot-toast";
+import { OrderStatus } from "@/app/interfaces/Order";
+import { useStaticData } from "./StaticDataContext";
+import Select from "react-select"
+
+interface OptionType {
+  value: string;
+  label: string;
+}
+
+type SortField = 'price' | 'orderDate' | 'deliveryDate' | 'quantity';
+type SortOrder = 'asc' | 'desc';
 
 export default function OrderHistory({ urlUserName }: { urlUserName: string }) {
+  const { items } = useStaticData();
+  const itemNameSelectOptions: OptionType[] = items.map(item => ({
+    value: item.name,
+    label: item.name
+  }));
   const [orders, setOrders] = useState<Order[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [filterOrderStatus, setFilterOrderStatus] = useState<string>("ALL");
+  const [minOrderDate, setMinOrderDate] = useState<Date>(new Date(2025, 0, 1));
+  const [maxOrderDate, setMaxOrderDate] = useState<Date>(new Date());
+  const [minDeliveryDate, setMinDeliveryDate] = useState<Date>(new Date(2025, 0, 1));
+  const [maxDeliveryDate, setMaxDeliveryDate] = useState<Date>(new Date());
+  const [sortField, setSortField] = useState<SortField>('price');
+  const [sortOrder, setSortOrder] = useState<SortOrder>('asc');
+  const [filterItemNames, setFilterItemName] = useState<string[] | null>(null);
+
+  function handleItemNameFilterChange(selectedNames: OptionType[] | null) {
+    const itemNames = selectedNames ? selectedNames.map(option => option.value) : [];
+    setFilterItemName(itemNames);
+  }
+
+  const handleSortFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSortField(e.target.value as SortField);
+  };
+
+  const handleSortOrderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSortOrder(e.target.value as SortOrder);
+  };
 
   useEffect(() => {
     async function fetchUserHistory() {
@@ -32,14 +69,161 @@ export default function OrderHistory({ urlUserName }: { urlUserName: string }) {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div className="flex flex-col justify-center items-center gap-4 p-4">
-      {orders.length > 0 ? (
-        orders.map(order => (
-          <OrderHistoryCard key={order.id} order={order} />
-        ))
-      ) : (
-        <div>No orders found.</div>
-      )}
+    <div className="grid grid-cols-2 grid-cols-[17%_auto] gap-4 h-full">
+      <aside className="p-2 flex flex-col shadow-lg bg-[var(--white)] text-[var(--black)]">
+
+        <div className="p-4">
+          <h2 className="text-lg font-bold mb-2">Sort By</h2>
+          <div className="flex flex-col gap-2">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortField"
+                value="price"
+                checked={sortField === 'price'}
+                onChange={handleSortFieldChange}
+              />
+              <span className="ml-2">Price</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortField"
+                value="orderDate"
+                checked={sortField === 'orderDate'}
+                onChange={handleSortFieldChange}
+              />
+              <span className="ml-2">Order Date</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortField"
+                value="deliveryDate"
+                checked={sortField === 'deliveryDate'}
+                onChange={handleSortFieldChange}
+              />
+              <span className="ml-2">Delivery Date</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortField"
+                value="quantity"
+                checked={sortField === 'quantity'}
+                onChange={handleSortFieldChange}
+              />
+              <span className="ml-2">Quantity</span>
+            </label>
+          </div>
+
+          <h2 className="text-lg font-bold mt-4 mb-2">Sort Order</h2>
+          <div className="flex items-center gap-4">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortOrder"
+                value="asc"
+                checked={sortOrder === 'asc'}
+                onChange={handleSortOrderChange}
+              />
+              <span className="ml-2">Ascending</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="sortOrder"
+                value="desc"
+                checked={sortOrder === 'desc'}
+                onChange={handleSortOrderChange}
+              />
+              <span className="ml-2">Descending</span>
+            </label>
+          </div>
+        </div>
+
+        <h2 className="font-bold mb-2">Order status</h2>
+        <select className="p-2 border rounded  bg-[var(--white)]"
+          value={filterOrderStatus}
+          onChange={(e) => setFilterOrderStatus(e.target.value)}
+        >
+          <option key={"ALL"} value={"ALL"}>
+            {"ALL"}
+          </option>
+          {Object.values(OrderStatus).map((status: string) => (
+            <option key={status} value={status}>
+              {status}
+            </option>
+          ))}
+        </select>
+
+
+        <h2 className="font-bold mb-2 my-2">Items</h2>
+        <Select
+          isMulti
+          options={itemNameSelectOptions}
+          onChange={handleItemNameFilterChange}
+          placeholder="Select item names..."
+        />
+
+
+        <h2 className="font-bold mb-2 my-2">Order Date</h2>
+        <div className="flex gap-4">
+          <div>
+            <label className="block font-semibold">From</label>
+            <input
+              className="border rounded bg-[var(--white)] p-1"
+              type="date"
+              value={minOrderDate.toISOString().substring(0, 10)}
+              onChange={(e) => setMinOrderDate(new Date(e.target.value))}
+            />
+          </div>
+          <div>
+            <label className="block font-semibold">To</label>
+            <input
+              className="border rounded bg-[var(--white)] p-1"
+              type="date"
+              value={maxOrderDate.toISOString().substring(0, 10)}
+              onChange={(e) => setMaxOrderDate(new Date(e.target.value))}
+            />
+          </div>
+        </div>
+
+        <h2 className="font-bold mb-2 my-2">Delivery Date</h2>
+        <div className="flex gap-4">
+          <div>
+            <label className="block font-semibold">From</label>
+            <input
+              className="border rounded bg-[var(--white)] p-1"
+              type="date"
+              value={minDeliveryDate.toISOString().substring(0, 10)}
+              onChange={(e) => setMinDeliveryDate(new Date(e.target.value))}
+            />
+          </div>
+          <div>
+            <label className="block font-semibold">To</label>
+            <input
+              className="border rounded bg-[var(--white)] p-1"
+              type="date"
+              value={maxDeliveryDate.toISOString().substring(0, 10)}
+              onChange={(e) => setMaxDeliveryDate(new Date(e.target.value))}
+            />
+          </div>
+        </div>
+
+
+
+      </aside>
+
+      <div className="flex flex-col justify-center items-center gap-4 p-4">
+        {orders.length > 0 ? (
+          orders.map(order => (
+            <OrderHistoryCard key={order.id} order={order} />
+          ))
+        ) : (
+          <div>No orders found.</div>
+        )}
+      </div>
     </div>
   );
 }
