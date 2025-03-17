72b2f681319631dbb96aefd942735b37794efbe7
jonathan <joni7840850@gmail.com>
2025-03-17 15:29:44 -0600

    Move try to just query

--- back/app/profile/ProfileWorker.py
+++ back/app/profile/ProfileWorker.py
@@ -1,5 +1,5 @@
-from back.app.customExceptions import ProfileWorkerException, UserNoGoldRow
-from back.app.data.queries.profileQueries import getUserGoldWithUserName
+from app.customExceptions import ProfileWorkerException, UserNoGoldRow
+from app.data.queries.profileQueries import getCurrentUserGoldWithUserName
 from app.logger import logger
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -10,16 +10,16 @@ class ProfileWorker:
         pass
 
     async def getUserGoldWithUserName(self,userName:str)->int:
+        logger.debug(f"Getting {userName} gold from database")
         try:
-            logger.debug(f"Getting {userName} gold from database")
-            gold : int | None = await getUserGoldWithUserName(self.dbSession, userName)
-            if gold is None:
-                logger.error(f"Error, the user {userName} has no gold in row in the database")
-                raise UserNoGoldRow()
-            logger.debug(f"{userName} has {gold} in the database")
-            return gold
+            gold : int | None = await getCurrentUserGoldWithUserName(self.dbSession, userName)
         except SQLAlchemyError as e:
             logger.error(f"Error, sqlalchemy error: {e}")
             raise ProfileWorkerException("SQLAlchemyError") from e
         except Exception as e:
             raise ProfileWorkerException("Unexpected exception while getting user gold") from e
+        if gold is None:
+            logger.error(f"Error, the user {userName} has no gold in row in the database")
+            raise UserNoGoldRow()
+        logger.debug(f"{userName} has {gold} in the database")
+        return gold
