00c39c3b277a64f71fb0f4215a94cdf9f1a76526
jonathan <joni7840850@gmail.com>
2025-03-16 21:38:41 -0600

    Add columns to database

--- back/app/data/models/UserTable.py
+++ back/app/data/models/UserTable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Text, String
+from sqlalchemy import Date, Text, String
 from sqlalchemy.orm import Mapped, mapped_column
 from app.data.database import base
 
@@ -9,3 +9,9 @@ class UserTable(base):
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     userName: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(Text, nullable=False)
+    created: Mapped[Date] = mapped_column(Date, nullable=False)
+    last_singn: Mapped[Date] = mapped_column(Date, nullable=False)
+    gold_spend: Mapped[int] = mapped_column(nullable=False, default=0)
+    current_gold: Mapped[int] = mapped_column(nullable=False, default=0)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    birthdate: Mapped[Date] = mapped_column(Date, nullable=False)
