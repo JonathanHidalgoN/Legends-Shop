e68daabcaf991cd7973629c82cbd2b59a07b7706
jonathan <joni7840850@gmail.com>
2025-03-16 21:48:24 -0600

    Add page and component for profile page

---
+++ front/src/app/components/ProfileView.tsx
@@ -0,0 +1,4 @@
+export default function ProfileView({ userName }: { userName: string }) {
+  return (<div>
+  </div>)
+}
---
+++ front/src/app/profile/[user_name]/page.tsx
@@ -0,0 +1,13 @@
+import ProfileView from "@/app/components/ProfileView";
+
+export default async function ProfilePage({
+  params,
+}: {
+  params: Promise<{ user_name: string }>;
+}) {
+  // Replace %20 with a space.
+  const urlUserName = (await params).user_name.replace(/%20/g, " ");
+  return (<div>
+    <ProfileView userName={urlUserName} />
+  </div>)
+}
