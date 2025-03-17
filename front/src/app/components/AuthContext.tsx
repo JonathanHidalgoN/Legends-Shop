bc55bfa5e9665d916c85349a47810e056c623aec
jonathan <joni7840850@gmail.com>
2025-03-17 12:55:56 -0600

    Add funciton to check singup error in header response and create an api response

--- front/src/app/components/AuthContext.tsx
+++ front/src/app/components/AuthContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { logInRequest, logoutRequest, refreshTokenRequest, singupRequest } from "../request";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
+import { APIResponse, SingupError } from "../interfaces/APIResponse";
 
 interface AuthContextType {
   userName: string | null;
@@ -12,7 +13,7 @@ interface AuthContextType {
   logOut: () => void;
   refreshToken: () => Promise<void>;
   singup: (userName: string, password: string,
-    email: string, birthDate: Date) => Promise<number>;
+    email: string, birthDate: Date) => Promise<APIResponse>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -42,24 +43,62 @@ export function AuthContextProvider({
     return response.status;
   }
 
+  function createAPIResponseSingup(response: Response, data: any): APIResponse {
+    if (!response.ok) {
+      if (!data) {
+        throw new Error("You need the data to create the api response if the status is not ok");
+      } else {
+        const errorTypeHeader: string | null = response.headers.get("X-Error-Type");
+        console.log("Here");
+        let errorType: SingupError | null = null;
+        if (errorTypeHeader === SingupError.EMAILEXIST) {
+          errorType = SingupError.EMAILEXIST
+        }
+        if (errorTypeHeader === SingupError.INVALIDEMAIL) {
+          errorType = SingupError.INVALIDEMAIL
+        }
+        if (errorTypeHeader === SingupError.USERNAMEEXIST) {
+          errorType = SingupError.USERNAMEEXIST
+        }
+        if (errorTypeHeader === SingupError.INVALIDDATE) {
+          errorType = SingupError.INVALIDDATE
+        }
+        return {
+          status: response.status,
+          errorType: errorType,
+          message: data.detail ? data.detail : "Error"
+        };
+      }
+    } else {
+      return {
+        status: response.status,
+        errorType: null,
+        message: "Success"
+      };
+    }
+  }
+
   async function singup(userName: string, password: string,
-    email: string, birthDate: Date): Promise<number> {
+    email: string, birthDate: Date): Promise<APIResponse> {
     const response = await singupRequest(userName, password,
       email, birthDate,
       "client");
     if (!response.ok) {
-      if (response.status == 400) {
-        toast.error("The username already exist, chose other");
-      } else if (response.status == 500) {
+      const data = await response.json();
+      const result: APIResponse = createAPIResponseSingup(response, data);
+      if (result.status == 400) {
+        toast.error(result.message);
+      } else if (result.status == 500) {
         toast.error("Internal server error login");
       } else {
         toast.error("Unexpected error");
       }
-      return response.status;
+      return result;
     }
+    const result: APIResponse = createAPIResponseSingup(response, null);
     toast.success(`Singup succesfully ${userName}!`);
     await login(userName, password);
-    return response.status;
+    return result;
   }
 
   async function refreshToken(): Promise<void> {
