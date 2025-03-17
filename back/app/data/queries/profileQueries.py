61cf19154de5dbb0d1cd8e8d742ddc92f9991655
jonathan <joni7840850@gmail.com>
2025-03-17 14:10:30 -0600

    Add profile query to get gold

---
+++ back/app/data/queries/profileQueries.py
@@ -0,0 +1,14 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.data.models.UserTable import UserTable
+
+
+async def getUserGoldWithUserName(asyncSession: AsyncSession, userName: str) -> int | None:
+    """
+    """
+    result = await asyncSession.execute(
+        select(UserTable.current_gold).where(UserTable.userName == userName)
+    )
+    userGold: int | None = result.scalars().first()
+    return userGold 
