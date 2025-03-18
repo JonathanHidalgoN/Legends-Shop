54954b22d8d1d3482eaeceeeaa3529ca4107e910
jonathan <joni7840850@gmail.com>
2025-03-18 09:44:39 -0600

    Add function to get current user gold

---
+++ front/src/app/profileFunctions.ts
@@ -0,0 +1,13 @@
+import toast from "react-hot-toast";
+import { getCurrentUserGoldRequest } from "./request";
+
+export async function getCurrentUserGold(): Promise<number | null> {
+  const response = await getCurrentUserGoldRequest("client");
+  if (!response.ok) {
+    toast.error("Server error while getting current user gold")
+    return null;
+  } else {
+    const data = await response.json();
+    return data.userGold;
+  }
+}
