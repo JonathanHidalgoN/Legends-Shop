d003c0712810682d9e70827852b09d837ac0704c
jonathan <joni7840850@gmail.com>
2025-03-18 09:42:25 -0600

    Add query to update current gold

--- back/app/data/queries/profileQueries.py
+++ back/app/data/queries/profileQueries.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.data.models.UserTable import UserTable
@@ -22,3 +23,16 @@ async def getCurrentUserGoldWithUserId(asyncSession: AsyncSession, userId: int) 
     )
     userGold: int | None = result.scalars().first()
     return userGold 
+
+
+async def updateUserGoldWithUserId(asyncSession: AsyncSession, userId: int, newGold: int) -> None:
+    """
+    """
+    result = await asyncSession.execute(
+        update(UserTable)
+        .where(UserTable.id == userId)
+        .values(current_gold=newGold)
+    )
+    if result.rowcount == 0:
+        raise SQLAlchemyError(f"Tried to update current_gold row of table UserTable with user id {userId} but 0 rows where updated")
+    await asyncSession.commit()
