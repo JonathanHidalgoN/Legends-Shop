0281e944fe0a1b5a7bf67388684bb31af675e923
jonathan <joni7840850@gmail.com>
2025-03-19 09:05:56 -0600

    Add validation to schema

--- back/app/schemas/AuthSchemas.py
+++ back/app/schemas/AuthSchemas.py
@@ -1,15 +1,34 @@
+import re
 from enum import Enum
-from pydantic import BaseModel
+from typing import Annotated
+from pydantic import AfterValidator, BaseModel
 from datetime import date
 
+from app.customExceptions import InvalidUserEmailException, InvalidUserGoldFieldException, InvalidUserNameException
+
+def userNameValidation(userName:str)->str:
+    MIN_LEN : int = 8 
+    if len(userName) < MIN_LEN:
+        raise InvalidUserNameException(f"Error, userName has to be {MIN_LEN} characters at least")
+    return userName
+
+def emailValidation(email:str)->str:
+    if re.match(r"^[^\s@]+@[^\s@]+\.[^\s@]+$",email):
+        return email
+    raise InvalidUserEmailException(f"Email {email} do not match email pattern")
+
+def isPositive(number:int)->int:
+    if number >=0:
+        return number
+    raise InvalidUserGoldFieldException(f"Number {number} should be positive")
 
 class User(BaseModel):
-    userName: str
-    email: str
+    userName: Annotated[str, AfterValidator(userNameValidation)] 
+    email: Annotated[str, AfterValidator(emailValidation)] 
     created: date
     lastSingIn: date
-    goldSpend: int
-    currentGold: int
+    goldSpend: Annotated[int, AfterValidator(isPositive)] 
+    currentGold: Annotated[int, AfterValidator(isPositive)]
     birthDate: date
 
 
@@ -31,3 +50,11 @@ class SingUpError(str, Enum):
     EMAILEXIST = "EMAILEXIST"
     INVALIDEMAIL = "INVALIDEMAIL"
     INVALIDDATE = "INVALIDDATE"
+    INVALIDUSERNAME = "INVALIDUSERNAME"
+    INVALIDUSERGOLD = "INVALIDUSERGOLD"
+
+class LogInError(str, Enum):
+    INVALIDUSERNAME = "INVALIDUSERNAME"
+    INVALIDPASSWORD = "INVALIDPASSWORD"
+    INCORRECTCREDENTIALS = "INCORRECTCREDENTIALS"
+    INTERNALSERVERERROR="INTERNALSERVERERROR"
