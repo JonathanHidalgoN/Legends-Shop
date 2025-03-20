e3d19be95ff35190a81882e4ec582e41af4b7394
jonathan <joni7840850@gmail.com>
2025-03-20 07:56:55 -0600

    Make all the component clickable except the add to car button

--- front/src/app/components/ItemPreView.tsx
+++ front/src/app/components/ItemPreView.tsx
@@ -4,19 +4,22 @@ import Link from "next/link";
 import { Item } from "../interfaces/Item";
 import DescriptionTextMapper from "./DescriptionTextMapper";
 import AddToCarButton from "./AddToCarButton";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import EpicLegendMap from "./EpicLegendMap";
 
 export default function ItemPreView({ item }: { item: Item }) {
   const targetLink = `/items/${item.name}`;
+  const router = useRouter();
+
+  const handleCardClick = () => {
+    router.push(targetLink);
+  };
 
   return (
     <div
-      className="flex flex-col w-full
-  md:flex-row items-start p-8 border border-black items-center
-  rounded-lg shadow-sm my-4 
-  hover:shadow-xl hover:scale-105 hover:border-gold 
-      hover:bg-gray-50 transition-all duration-300 ease-in-out"
+      onClick={handleCardClick}
+      className="flex flex-col w-full md:flex-row items-start p-8 border border-black items-center rounded-lg shadow-sm my-4 hover:shadow-xl hover:scale-105 hover:border-gold hover:bg-gray-50 transition-all duration-300 ease-in-out cursor-pointer"
     >
       <div className="flex-shrink-0 ">
         <Link href={targetLink}>
@@ -32,7 +35,7 @@ export default function ItemPreView({ item }: { item: Item }) {
       </div>
 
       <div className="flex-1 md:ml-4 mt-4 md:mt-0">
-        <h3 className={`text-xl font-bold text-[var(--black)]`}>{item.name}</h3>
+        <h3 className="text-xl font-bold text-[var(--black)]">{item.name}</h3>
         <div className="m-2">
           <EpicLegendMap itemName={item.name} />
         </div>
@@ -43,8 +46,7 @@ export default function ItemPreView({ item }: { item: Item }) {
             {item.tags.map((tag, index) => (
               <span
                 key={index}
-                className="inline-block bg-orange-200 
-                text-black text-xs px-2 py-1 rounded mr-2 mt-1"
+                className="inline-block bg-orange-200 text-black text-xs px-2 py-1 rounded mr-2 mt-1"
               >
                 {tag}
               </span>
@@ -58,8 +60,7 @@ export default function ItemPreView({ item }: { item: Item }) {
             {item.effects.map((effect, index) => (
               <span
                 key={index}
-                className="inline-block bg-[var(--pink2)]
-                text-black text-xs px-2 py-1 rounded mr-2 mt-1"
+                className="inline-block bg-[var(--pink2)] text-black text-xs px-2 py-1 rounded mr-2 mt-1"
               >
                 {effect.name} ({effect.value})
               </span>
@@ -74,7 +75,7 @@ export default function ItemPreView({ item }: { item: Item }) {
             ${item.gold.total}
           </span>
         </div>
-        <div className="mt-2">
+        <div className="mt-2" onClick={(e) => e.stopPropagation()}>
           <AddToCarButton item={item} />
         </div>
       </div>
