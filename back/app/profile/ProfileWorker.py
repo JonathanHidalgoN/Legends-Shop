c7be7a89cc1a25de27ef295ee617fed12f13f8aa
jonathan <joni7840850@gmail.com>
2025-03-21 09:04:26 -0600

    Add methods to get items ordered by user and build order summary

--- back/app/profile/ProfileWorker.py
+++ back/app/profile/ProfileWorker.py
@@ -1,7 +1,14 @@
+from typing import List, Tuple
 from app.customExceptions import ProfileWorkerException, UserNoGoldRow
 from app.data.queries.profileQueries import getCurrentUserGoldWithUserName
 from app.logger import logger
 from sqlalchemy.exc import SQLAlchemyError
+
+from app.data.queries.authQueries import getUserInDB
+from app.data.queries.orderQueries import getUniqueItemNamesQuantityAndBasePriceByUserName
+from app.schemas.AuthSchemas import UserInDB
+from app.schemas.Order import OrderSummary
+from app.schemas.profileSchemas import ProfileInfo
 
 
 class ProfileWorker:
@@ -30,3 +37,29 @@ class ProfileWorker:
             raise UserNoGoldRow()
         logger.debug(f"{userName} has {gold} in the database")
         return gold
+
+    async def getProfileInfo(self, userName:str) -> ProfileInfo:
+        logger.debug(f"Getting {userName} profile info from database")
+        try:
+            user: UserInDB | None = await getUserInDB(self.dbSession, userName)
+            if user is None:
+                logger.error(f"{userName} information can not be None, this a internal error")
+                raise ProfileWorkerException("None value in user info")
+            orderSummaryList: List[OrderSummary] = await self.buildOrderSummaryForUser(userName)
+            return ProfileInfo(user=user, ordersInfo=orderSummaryList)
+        except SQLAlchemyError as e:
+            logger.error(f"Error getting user {userName} information, exception: {e}")
+            raise ProfileWorkerException("SQLAlchemyError") from e
+
+    async def buildOrderSummaryForUser(self, userName:str) -> List[OrderSummary]:
+        logger.debug(f"Building {userName} order summary")
+        try:
+            logger.debug(f"Getting {userName} ordered items and base price")
+            itemNameAndCostOrderedByUser : List[OrderSummary] = await getUniqueItemNamesQuantityAndBasePriceByUserName(self.dbSession, userName)
+            logger.debug(f"{userName} ordered summary and base price info obtained successfully")
+            return itemNameAndCostOrderedByUser
+        except SQLAlchemyError as e:
+            logger.debug(f"Getting {userName} profile info from database")
+            raise ProfileWorkerException("SQLAlchemyError") from e
+
+
