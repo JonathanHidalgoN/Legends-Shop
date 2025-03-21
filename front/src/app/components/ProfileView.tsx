b08254981bf7fe077663f443ec47a1bbfd0b2ad9
jonathan <joni7840850@gmail.com>
2025-03-21 09:05:57 -0600

    Add hook to get profile info in profile page

--- front/src/app/components/ProfileView.tsx
+++ front/src/app/components/ProfileView.tsx
@@ -1,3 +1,23 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { ProfileInfo } from "../interfaces/APIResponse";
+import { getProfileInfo } from "../profileFunctions";
+
 export default function ProfileView({ userName }: { userName: string }) {
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [profileInfo, setProfileInfo] = useState<ProfileInfo | null>(null);
+
+  useEffect(() => {
+    async function fetchProfileInfo() {
+      const profileInfo: ProfileInfo | null = await getProfileInfo()
+      setProfileInfo(profileInfo);
+    }
+    fetchProfileInfo();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
   return <div></div>;
 }
