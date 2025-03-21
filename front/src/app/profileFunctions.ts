a9cb893d2594ef9de430a39a803dd428705bf38f
jonathan <joni7840850@gmail.com>
2025-03-21 09:06:46 -0600

    Add function to get profile info

--- front/src/app/profileFunctions.ts
+++ front/src/app/profileFunctions.ts
@@ -1,5 +1,6 @@
 import toast from "react-hot-toast";
-import { getCurrentUserGoldRequest } from "./request";
+import { getCurrentUserGoldRequest, getProfileInfoRequest } from "./request";
+import { ProfileInfo } from "./interfaces/APIResponse";
 
 export async function getCurrentUserGold(): Promise<number | null> {
   const response = await getCurrentUserGoldRequest("client");
@@ -9,5 +10,16 @@ export async function getCurrentUserGold(): Promise<number | null> {
   } else {
     const data = await response.json();
     return data.userGold;
+  }
+}
+
+export async function getProfileInfo(): Promise<ProfileInfo | null> {
+  const response = await getProfileInfoRequest("client");
+  if (!response.ok) {
+    toast.error("Server error while getting current user gold");
+    return null;
+  } else {
+    const data = await response.json();
+    return data.profileInfo;
   }
 }
