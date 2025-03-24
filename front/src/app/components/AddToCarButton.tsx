5936c9ad3fec12cc16926cd4e0776b5d270c7094
jonathan <joni7840850@gmail.com>
2025-03-24 08:01:45 -0600

    Move logic to car context

--- front/src/app/components/AddToCarButton.tsx
+++ front/src/app/components/AddToCarButton.tsx
@@ -1,41 +1,15 @@
 import { Item } from "../interfaces/Item";
 import { useCarContext } from "./CarContext";
-import { showSuccessToast } from "../customToast";
-import { CartItem, CartStatus } from "../interfaces/Order";
-import { APICartItemResponse } from "../interfaces/APIResponse";
-import useSWR from "swr";
-import { addToCarRequest } from "../request";
-import { useErrorRedirect } from "./useErrorRedirect";
-import { mapAPICartItemResponseToCartItem } from "../mappers";
-import { useState } from "react";
 
 export default function AddToCarButton({ item }: { item: Item }) {
   const { addOneItemToCar } = useCarContext();
-
-  async function handleAddToCarButton() {
-    const apiCartItem: APICartItemResponse = {
-      id: null,
-      status: CartStatus.PENDING,
-      itemId: item.id
-    };
-    try {
-      console.log("here");
-      const data: APICartItemResponse = await addToCarRequest("client", apiCartItem);
-      const cartItem: CartItem = mapAPICartItemResponseToCartItem(apiCartItem, item);
-      addOneItemToCar(cartItem);
-      showSuccessToast(`${item.name} added to car`)
-    } catch (error) {
-
-    }
-
-  }
 
   return (
     <div>
       <button
         className="mt-6 bg-[var(--orange)] text-[var(--white)] 
       px-6 py-3 rounded hover:bg-[var(--pink1)] transition-colors"
-        onClick={handleAddToCarButton}
+        onClick={() => addOneItemToCar(item)}
       >
         Add to Car
       </button>
