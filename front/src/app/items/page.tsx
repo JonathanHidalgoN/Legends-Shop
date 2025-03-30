4fb0f9b5e8da5fe5e5ca2e38049c2af1c35c78ae
jonathan <joni7840850@gmail.com>
2025-03-30 12:12:20 -0600

    Add serach params as arg to load items

--- front/src/app/items/page.tsx
+++ front/src/app/items/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import SelectedItems from "../components/SelectedItems";
 import { useStaticData } from "../components/StaticDataContext";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function ItemsPage() {
   const { items, tags, effects } = useStaticData();
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get('search');
 
   if (!items || items.length === 0) {
     router.push("/error/wrong");
@@ -17,5 +19,5 @@ export default function ItemsPage() {
     router.push("/error/wrong");
   }
 
-  return <SelectedItems items={items} tags={tags} effects={effects} />;
+  return <SelectedItems items={items} tags={tags} effects={effects} initialSearch={searchQuery} />;
 } 
