1d2f21f42329c1090a16cdfb47f9beb7afb86505
jonathan <joni7840850@gmail.com>
2025-04-04 10:40:54 -0600

    add method to get reviews by user id

--- back/app/reviews/ReviewProcessor.py
+++ back/app/reviews/ReviewProcessor.py
@@ -3,9 +3,11 @@ from app.schemas.Review import Review
 from sqlalchemy.exc import SQLAlchemyError
 from app.auth.functions import logMethod
 from app.customExceptions import InvalidRatingException, InvalidUserReview, ReviewProcessorException
-from app.data.queries.reviewQueries import addReview
+from app.data.queries.reviewQueries import addReview, getReviewsByUserId, getCommentsByUserId
 from app.data.queries.reviewQueries import addComment
-from back.app.data.queries.orderQueries import getUserIdByOrderId
+from app.data.queries.orderQueries import getUserIdByOrderId, markOrderAsReviewed
+from app.data.mappers import mapReviewTableToReview, mapCommentTableToComment
+from typing import List
 
 
 class ReviewProcessor:
@@ -26,9 +28,30 @@ class ReviewProcessor:
                                            review.orderId, review.itemId, review.rating) 
             for comment in review.comments:
                 await addComment(self.dbSession, reviewId, userId, comment.content)
+
+            await markOrderAsReviewed(self.dbSession, review.orderId)
         except InvalidRatingException as e:
             await self.dbSession.rollback()
             raise e
         except (Exception,SQLAlchemyError) as e:
             await self.dbSession.rollback()
             raise ReviewProcessorException("Internal server error") from e
+            
+    @logMethod
+    async def getReviewsByUserId(self, userId: int) -> List[Review]:
+        """
+        Get all reviews and their comments for a specific user.
+        
+        Args:
+            userId (int): The ID of the user whose reviews to retrieve
+            
+        Returns:
+            List[Review]: A list of reviews with their associated comments
+        """
+        try:
+            reviews = await getReviewsByUserId(self.dbSession, userId)
+            mapped_reviews = [mapReviewTableToReview(review) for review in reviews]
+            
+            return mapped_reviews
+        except SQLAlchemyError as e:
+            raise ReviewProcessorException("Error retrieving reviews") from e
