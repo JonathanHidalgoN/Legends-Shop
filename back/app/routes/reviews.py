aed6c3ec5b1dbccb4d4d73a04a5eb8314b326fb3
jonathan <joni7840850@gmail.com>
2025-04-04 10:41:11 -0600

    Add route to add a review and get a reviews

--- back/app/routes/reviews.py
+++ back/app/routes/reviews.py
@@ -1,11 +1,11 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.data.database import getDbSession
 from app.schemas.Review import Review
 from app.reviews.ReviewProcessor import ReviewProcessor
 from app.customExceptions import InvalidUserReview, ReviewProcessorException, InvalidRatingException
-from back.app.routes.auth import getUserIdFromName
+from app.routes.auth import getUserIdFromName
 
 
 def getReviewProcessor(
@@ -17,7 +17,7 @@ def getReviewProcessor(
 router = APIRouter()
 
 
-@router.post("/", status_code=200)
+@router.post("/add", status_code=200)
 async def addReview(
     review: Review,
     userId: Annotated[int, Depends(getUserIdFromName)],
@@ -30,5 +30,22 @@ async def addReview(
         raise HTTPException(status_code=400, detail=str(e))
     except InvalidUserReview as e:
         raise HTTPException(status_code=401, detail=str(e))
+    except ReviewProcessorException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/user", status_code=200, response_model=List[Review])
+async def getReviewsByUserId(
+    userId: Annotated[int, Depends(getUserIdFromName)],
+    reviewProcessor: ReviewProcessor = Depends(getReviewProcessor),
+):
+    """
+    Get all reviews for the authenticated user.
+    
+    This endpoint retrieves all reviews and their associated comments for the currently authenticated user.
+    """
+    try:
+        reviews = await reviewProcessor.getReviewsByUserId(userId)
+        return reviews
     except ReviewProcessorException as e:
         raise HTTPException(status_code=500, detail=str(e)) 
