177b9eb6ec93e63a870707eed442e93f7638ce58
jonathan <joni7840850@gmail.com>
2025-04-05 13:22:13 -0600

    Fix test

--- back/test/unit/OrderStatusProcessor_unit_test.py
+++ back/test/unit/OrderStatusProcessor_unit_test.py
@@ -19,15 +19,17 @@ async def test_update_order_statuses_no_orders(processor):
 
     processor.asyncSession.execute = AsyncMock(return_value=mock_result)
 
-    await processor.update_order_statuses()
+    await processor.updateOrderStatuses()
     assert (
-        processor.asyncSession.execute.await_count == 2
-    )  # Just the two queries, no updates
-
+        #One query for pending orders tomorrow
+        #One query for pending orders today 
+        #One query to update shipped to delivered
+        processor.asyncSession.execute.await_count == 3 
+    )  
 
 @pytest.mark.asyncio
 async def test_update_order_statuses_db_error(processor):
     processor.asyncSession.execute = AsyncMock(side_effect=SQLAlchemyError("DB error"))
 
     with pytest.raises(SQLAlchemyError):
-        await processor.update_order_statuses()
+        await processor.updateOrderStatuses()
