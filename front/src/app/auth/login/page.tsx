4d991f32fea8a180a4d55e0f69250ec292fe1cb1
jonathan <joni7840850@gmail.com>
2025-04-12 20:54:22 -0600

    Add front suspense

--- front/src/app/auth/login/page.tsx
+++ front/src/app/auth/login/page.tsx
@@ -4,16 +4,22 @@ import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import LoginForm from "@/app/components/LoginForm";
 
-export default function LogInPage() {
+function LogInContent() {
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirect") || "/";
 
   return (
+    <div className="bg-[var(--white)] min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl text-[var(--orange)] font-bold mb-6">Login</h1>
+      <LoginForm redirectPath={redirect} />
+    </div>
+  );
+}
+
+export default function LogInPage() {
+  return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div className="bg-[var(--white)] min-h-screen flex flex-col items-center justify-center p-4">
-        <h1 className="text-3xl text-[var(--orange)] font-bold mb-6">Login</h1>
-        <LoginForm redirectPath={redirect} />
-      </div>
+      <LogInContent />
     </Suspense>
   );
 }
