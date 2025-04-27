f90b68f926aa11473480afe594b9014cd4e7c330
jonathan <joni7840850@gmail.com>
2025-04-26 20:08:27 -0600

    Add variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,18 @@ variable "vnet_name" {
   default = "myTFVnet"
 }
 
+variable "service_plan_name" {
+  default = "legendsShop-service_plan"
+}
+
+variable "web_app_name" {
+  default = "LegendsShop"
+}
+
 variable "location" {
   default = "mexicocentral"
+}
+
+variable "docker_image_name_backend" {
+  default = "jonathanhn/dockerhub:legeshop-backend"
 }
