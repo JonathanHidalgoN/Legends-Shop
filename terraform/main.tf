791dd8fbeea2fe96e5958ec3dc8c0c128a0a62ff
jonathan <joni7840850@gmail.com>
2025-04-26 20:08:16 -0600

    Add web app infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,59 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.27.0"
     }
   }
 
   required_version = ">= 1.1.0"
 }
 
+# Provider configuration - tells Terraform to use Azure as the provider
 provider "azurerm" {
   features {}
 }
 
+# Resource Group creation - all Azure resources need to be inside a resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 
   tags = {
-    Environment = "Legends shop"
+    Environment = "Legends Shop"
     Team        = "DevOps"
   }
 }
 
+# Service Plan creation - defines the hosting environment for the app
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = var.service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "F1" # F1 is the free tier
+}
+
+# Web App configuration - sets up the Azure Web App to use the Docker image
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = var.web_app_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  https_only            = true # Ensures that only HTTPS traffic is allowed
+  depends_on            = [azurerm_service_plan.appserviceplan] # Ensures app is deployed after the service plan
+
+  application_stack {
+    
+    docker_image_name = var.docker_image_name_backend
+
+  }
+}
+
+# (Optional) Virtual Network configuration - you could define a VNet here for security or scaling
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
