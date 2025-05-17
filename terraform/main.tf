76d7438b3c889fcafe46180a6309705fe8c8cbcf
jonathan <joni7840850@gmail.com>
2025-05-16 18:48:07 -0600

    Add state managment in the backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,14 @@ terraform {
   # Minimum required version of Terraform.
   required_version = ">= 1.1.0"
 
-  # TODO:Configure remote state backend (RECOMMENDED for production)
+  # Configure Azure backend for state management
+  backend "azurerm" {
+    resource_group_name  = "resource-group-ls-p-mexc-1"
+    storage_account_name = "lspmexc1storageacc54"
+    container_name       = "tfstate"
+    key                  = "prod/legends-shop/terraform.tfstate"
+  }
 }
-
 # Configure the Azure provider plugin.
 provider "azurerm" {
   features {}
