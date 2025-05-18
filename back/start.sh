81a231a163a9ffdfbd928707daf160461aad2e45
jonathan <joni7840850@gmail.com>
2025-05-17 18:06:56 -0600

    Give auth

--- back/start.sh
+++ back/start.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# echo "Running database migrations..."
-# alembic upgrade head
+echo "Running database migrations..."
+alembic upgrade head
 
 echo "Migrations complete. Starting application server..."
 uvicorn app.main:app --host 0.0.0.0 --port ${BACKEND_PORT:-8000} "$@"
